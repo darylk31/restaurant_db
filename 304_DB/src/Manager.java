829f4ae3e91d88e279cd060a6a31c7e2a872fa41
Daryl <darylkwok333@Gmail.com>
2017-03-30 02:40:12 -0700

    Test

--- 304_DB/src/Manager.java
+++ 304_DB/src/Manager.java
@@ -9,46 +9,6 @@ public class Manager {
 		oracleConnect = new OracleConnect();
 	}
 	
-	public ResultSet deliveryQuery(){
-		return oracleConnect.selectTable("delivery");
-	}
-	
-	public ResultSet dineInQuery(){
-		return oracleConnect.selectTable("dinein");
-	}
-	
-	public ResultSet takeOutQuery(){
-		return oracleConnect.selectTable("takeout");
-	}
-	
-	public ResultSet orderQuery(){
-		return oracleConnect.selectTable("order");
-	}
-	
-	public ResultSet inventoryQuery(){
-		return oracleConnect.selectTable("inventory");
-	}
-	
-	public ResultSet customerQuery(){
-		return oracleConnect.selectTable("customer");
-	}
-	
-	public ResultSet accountQuery(){
-		return oracleConnect.selectTable("account");
-	}
-	
-	public ResultSet kitchenQuery(){
-		return oracleConnect.selectTable("kitchen");
-	}
-	
-	public ResultSet frontEndQuery(){
-		return oracleConnect.selectTable("frontend");
-	}
-	
-	public ResultSet delivererQuery(){
-		return oracleConnect.selectTable("deliverer");
-	}
-	
 	public ResultSet findEmployee(String id) throws SQLException{
 		if (id.isEmpty()) {
 			return oracleConnect.selectAll("employee");
