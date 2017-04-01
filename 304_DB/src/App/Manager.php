7cad693c21d15aff642173145ca5671955b73b87
ShadowWyan <ShadowWyan@hotmail.com>
2017-03-31 18:41:55 -0700

    got select to work

--- 304_DB/src/App/Manager.php
+++ 304_DB/src/App/Manager.php
@@ -2,7 +2,7 @@
 	<option value="Manager.php">Manager</option>
 	<option value="Employee.php">Employee</option>
 	<option value="Customer.php">Customer</option>
-</select>	
+</select>
 <p><font size ="2"> Find employee:</font></p>
 <form method="POST" action="Manager.php">
 <!--refresh page when submit-->
@@ -96,13 +96,51 @@ function executePlainSQL($cmdstr) { //takes a plain (no bound variables) SQL com
 
 }
 
+function executeBoundSQL($cmdstr, $list) {
+	/* Sometimes a same statement will be excuted for severl times, only
+	 the value of variables need to be changed.
+	 In this case you don't need to create the statement several times; 
+	 using bind variables can make the statement be shared and just 
+	 parsed once. This is also very useful in protecting against SQL injection. See example code below for       how this functions is used */
+
+	global $db_conn, $error;
+	$statement = OCIParse($db_conn, $cmdstr);
+
+	if (!$statement) {
+		echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+		$e = OCI_Error($db_conn);
+		echo htmlentities($e['message']);
+		$error = True;
+	}
+
+	foreach ($list as $tuple) {
+		foreach ($tuple as $bind => $val) {
+			//echo $val;
+			//echo "<br>".$bind."<br>";
+			OCIBindByName($statement, $bind, $val);
+			unset ($val); //make sure you do not remove this. Otherwise $val will remain in an array object wrapper which will not be recognized by Oracle as a proper datatype
+
+		}
+		$r = OCIExecute($statement, OCI_DEFAULT);
+		if (!$r) {
+			echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+			$e = OCI_Error($statement); // For OCIExecute errors pass the statementhandle
+			echo htmlentities($e['message']);
+			echo "<br>";
+			$error = True;
+		}
+	}
+
+}
+
+
 function printResult($result) { //prints results from a select statement
 	echo "<br>Got data from table employee:<br>";
 	echo "<table>";
 	echo "<tr><th>ID</th><th>Name</th></tr>";
 
 	while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
-		echo "<tr><td>" . $row["ID"] . "</td><td>" . $row["NAME"] . "</td></tr>"; //or just use "echo $row[0]"
+		echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td></tr>"; //or just use "echo $row[0]"
 	}
 	echo "</table>";
 
@@ -110,17 +148,13 @@ function printResult($result) { //prints results from a select statement
 
 if ($db_conn) {
 
-	if (array_key_exists('doStuff', $_POST) {
-		$tuple = array(
-				":bind1" => $_POST['eId'],
-				":bind2" => $_POST('table']
-		);
-		$result = executeBoundSQL("select * from :bind2 where id = :bind1", $tuple);
+	if (array_key_exists('doStuff', $_POST)) {
+		$result = executePlainSQL("select * from " . $_POST['table'] . " where id =" . $_POST['eId'] ."");
 		printResult($result);
 	}
-
-echo "<br>Started Connection<br>";
-	if ($_POST && !$error) {
+	
+	echo "<br>Started Connection<br>";
+	if ($_POST && $error) {
 		//POST-REDIRECT-GET -- See http://en.wikipedia.org/wiki/Post/Redirect/Get
 		header("location: Manager.php");
 	} else {
